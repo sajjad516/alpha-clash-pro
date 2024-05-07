f9f4ed4094e916861a242c1f4a6b9d6ec909e736
sajjad516 <mohammedsajjaduli9@gmail.com>
2024-05-07 15:11:36 +0600

    letter game finished

--- js/script.js
+++ js/script.js
@@ -1,15 +1,42 @@
-/* function play(){
-    const homeScreen = document.getElementById('home-screen');
-    homeScreen.classList.add('hidden');
 
-    const playGround = document.getElementById('play-ground');
-    playGround.classList.remove('hidden');
-} */
+function handelKeyboardKeyPress(event) {
+    const playerPressed = event.key;
 
-function continueGame(){
+    if(playerPressed === 'Escape'){
+        gameOver();
+    }
+
+    //get expected to press
+    const currentAlphabetElement = document.getElementById('current-alpha');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+
+    //check matched or not
+    if (playerPressed === expectedAlphabet) {
+
+        const currentScore = getTextElementById('current-score');
+        const updateScore = currentScore + 1;
+        setTextElementValueById('current-score', updateScore);
+
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    } else {
+        const currentLife = getTextElementById('current-life');
+        const newLife = currentLife - 1;
+        const life = reduceValueById('current-life', newLife);
+        if (newLife === 0) {
+            gameOver();
+        }
+    }
+}
+
+//capture keyboard press
+document.addEventListener('keyup', handelKeyboardKeyPress)
+
+function continueGame() {
     //step-1: generate a random alphabet
     const alphabet = getARandomAlphabet();
-    // console.log(alphabet)
+    
     //set randomly generated alphabet to the screen (show it)
     const currentAlphabetElement = document.getElementById('current-alpha');
     currentAlphabetElement.innerText = alphabet
@@ -17,8 +44,29 @@ function continueGame(){
     setBackgroundColorById(alphabet);
 }
 
-function play(){
+function play() {
+    //everything hide and show only playground
     hideElementById('home-screen');
+    hideElementById('final-score');
     showElementById('play-ground');
+
+    //reset score and life
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
+
     continueGame();
+}
+
+function gameOver() {
+    hideElementById('play-ground');
+    showElementById('final-score');
+
+    //set the last score
+    const lastScore = getTextElementById('current-score');
+    setTextElementValueById('game-score', lastScore);
+
+    //clear the last selected alphabet
+    const currentAlphabet = getElementTextById('current-alpha');
+    // console.log(currentAlphabet)
+    removeBackgroundColorById(currentAlphabet);
 }
--- js/utility.js
+++ js/utility.js
@@ -15,18 +15,44 @@ function setBackgroundColorById(elementId){
     element.classList.add('bg-orange-400')
 }
 
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+
+function getTextElementById(elementId){
+    const element = document.getElementById(elementId);
+    const elementText = element.innerText;
+    const value = parseInt(elementText);
+    return value;
+}
+
+function setTextElementValueById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
+function getElementTextById(elementId){
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
+}
+
+function reduceValueById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
 function getARandomAlphabet(){
     //crate an alphabet array
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = alphabetString.split('');
-    // console.log(alphabets);
 
     //get a random index between 0 - 25
     const radomNumber = Math.random()* 25;
     const index = Math.round(radomNumber);
-    // console.log(index)
 
     const alphabet = alphabets[index];
-    // console.log(index, alphabet)
+    
     return alphabet;
 }
